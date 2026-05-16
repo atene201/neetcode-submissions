6d28d56b94f91d2d95836bcda131fa4e453d65a3
Andres Tenesaca <atenesaca201@gmail.com>
2026-05-16 19:36:24 -0400

    Add: design-hashmap - submission-16

---
+++ Data Structures & Algorithms/design-hashmap/submission-16.py
@@ -0,0 +1,28 @@
+class Node:
+    
+    def __init__(self, key: int):
+        self.key = None
+        self.val = None
+
+class MyHashMap:
+
+    def __init__(self):
+        self.map = [None] * (10**6 + 1)
+
+    def put(self, key: int, value: int) -> None:
+        self.map[key] = value
+
+    def get(self, key: int) -> int:
+        val = self.map[key]
+        return val if val is not None else -1
+
+    def remove(self, key: int) -> None:
+        self.map[key] = None
+
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
