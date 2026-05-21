2be5d9bd3fcbbbeb4033fea1e565e6fe3a08f626
Andres Tenesaca <atenesaca201@gmail.com>
2026-05-20 21:41:51 -0400

    Add: find-pivot-index - submission-4

---
+++ Data Structures & Algorithms/find-pivot-index/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        # prefix sol
+        total_sum = sum(nums)
+        prefix_sum = [0] * (len(nums) + 1) # use a 1-indexed arr
+
+        for idx in range(1, len(nums)+1):
+            prefix_sum[idx] = nums[idx-1] + prefix_sum[idx-1] # use the prev sum
+            right_sum = total_sum - prefix_sum[idx]
+            if right_sum == prefix_sum[idx-1]:
+                return idx-1
+        return -1
