48bc90a6638fa86f1fb3a901dca66277b459db3b
Andres Tenesaca <atenesaca201@gmail.com>
2026-05-29 22:32:16 -0400

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+
+        left = 0
+        right = len(s)-1
+
+        while left < right:
+            if s[left] != s[right]:
+                sub_left = s[left + 1: right + 1]
+                sub_right = s[left : right]
+                return sub_left == sub_left[::-1] or sub_right == sub_right[::-1]
+            left += 1
+            right -= 1
+        return True
